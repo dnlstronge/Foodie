481a97c963be88d521bcaa643da586805adaf629
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 11:11:14 +0000

    [x] - add an array of dummy meals

---
+++ src/Components/Meals/AvailableMeals.js
@@ -0,0 +1,32 @@
+
+const DUMMY_MEALS = [
+     {
+        id: 'm1',
+        name: 'Sushi Bowl',
+        description: 'Finest fresh fish and vegetables',
+        price: 22.99,
+      },
+      {
+        id: 'm2',
+        name: 'Chickpea and Lentil Dahl',
+        description: 'Spicy and nutritious',
+        price: 16.5,
+      },
+      {
+        id: 'm3',
+        name: 'BBQ Burger',
+        description: 'An American meat beast',
+        price: 12.99,
+      },
+      {
+        id: 'm4',
+        name: 'Green Olive Salad',
+        description: 'Healthy...and green...',
+        price: 18.99,
+      },
+    ];
+
+const AvailableMeals = () => {
+
+}
+export default AvailableMeals;
