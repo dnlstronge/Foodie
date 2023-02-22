f83c4a779b3331f6cb227370e0bcd4f9213ea442
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 14:35:41 +0000

    [x] - create Cart component & dummy array

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,8 +1,24 @@
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+    const cartItems = 
+        <ul className={classes["cart-items"]}> 
+        {
+        [{id: "c1", name: "Pizza", amount: "2", price: "12.99"}].map(item => {<li>{item.name}</li>})
+        }
+        </ul>
+
     return (
-        <div></div>
+        <div>{cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>£69.69</span>
+            </div>
+            <div className={classes.actions}>
+                <button className={classes["button--alt"]}>Close</button>
+                <button className={classes.button}>Order</button>
+            </div>
+        </div>
     )
 }
 
