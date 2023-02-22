71ee3f094c84c7a358cd872c9075ffea63ab1e64
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 14:59:19 +0000

    [x] - set up portal for backdrop and modal overlay

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,8 +1,29 @@
+import { ReactDOM } from "react";
+import React, { Fragment }from "react";
 import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+    return (
+        <div classNam={classes.backdrop}></div>
+    )
+}
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    )
+}
+
+const portalElement = document.getElementById("overlays")
 
 const Modal = (props) => {
     return (
-        <sumthing></sumthing>
+        <Fragment>
+            {ReactDOM.createPortal( <Backdrop/>)}
+            {ReactDOM.createPortal( <ModalOverlay>{props.children}</ModalOverlay>)}
+        </Fragment>
     )
 };
 
