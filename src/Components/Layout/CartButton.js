50f1ac190ce7d4171a0c9a5a87f0cbbde944ddfe
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 10:36:59 +0000

    [x] - new component import CartIcon

--- src/Components/Layout/CartButton.js
+++ src/Components/Layout/CartButton.js
@@ -4,7 +4,9 @@ import classes from "./CartButton.module.css"
 const CartButton = (props) => {
     return (
         <Fragment>
-            
+            <span></span>
+            <span></span>
+            <span></span>
         </Fragment>
     )
 }
