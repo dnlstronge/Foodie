624c85882de9a80593c3710a6c2f50352233598a
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 10:57:45 +0000

    [x] - complete template

---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+    return ( 
+        <section className={classes.summary}>
+            <h2>Delicious Food Delievered Daily</h2>
+            <p>Choose your favourites from our wide selection of Tasty Treats!</p>
+            <p>Made with love, the finest of ingredients and the best locally sourced produce. Tasty Treats are
+            prepared fresh and onsite each morning by our experienced chefs! </p>
+        </section>
+    )
+}
+
+export default MealsSummary;
