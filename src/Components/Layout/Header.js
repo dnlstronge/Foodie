bb8d8342dd3d194c26a0fd001b6dedb39ee3d046
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 10:17:48 +0000

    [x] - create assets and import background

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,7 +3,13 @@ import { Fragment } from "react"
 const Header = (props) => {
     return (
         <Fragment>
-
+            <header>
+                <h1>Tasty Treats</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img src="../../Assets/meals.jpg" alt="Food background" />
+            </div>
         </Fragment>
     )
 }
