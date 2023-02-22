9d0c844b0e6ee81112cf0e5a23d3b0b98057a729
Dan Stronge <dnlstronge@gmail.com>
2023-02-22 10:43:02 +0000

    [x] - replace button with custom button

--- src/Components/Layout/CartButton.js
+++ src/Components/Layout/CartButton.js
@@ -1,12 +1,19 @@
 import React, { Fragment }  from "react"
 import classes from "./CartButton.module.css"
+import CartIcon from "../Cart/CartIcon"
 
 const CartButton = (props) => {
     return (
         <Fragment>
-            <span></span>
-            <span></span>
-            <span></span>
+            <button className={classes.button}>
+                <span className={classes.icon}>
+                    <CartIcon/>
+                </span>
+                <span >Your Cart</span>
+                <span className={classes.badge}>
+                    3
+                </span>
+            </button>
         </Fragment>
     )
 }
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react"
 import meals from "../../Assets/meals.jpeg"
 import classes from "./Header.module.css"
+import CartButton from "./CartButton"
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Tasty Treats</h1>
-                <button>Cart</button>
+                <CartButton />
             </header>
             <div className={classes["main-image"]}>
                 <img src={meals} alt="Table full of treats" />
