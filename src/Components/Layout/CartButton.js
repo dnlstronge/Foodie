0b97cedb6f3b70151a480a3406d4f43033aecfe9
Dan Stronge <dnlstronge@gmail.com>
2023-02-24 11:42:19 +0000

    [x] - add useEffect [x] - add state for useEffect [x] - hook up bump animation

--- src/Components/Layout/CartButton.js
+++ src/Components/Layout/CartButton.js
@@ -1,18 +1,23 @@
-import React, { useContext, Fragment }  from "react"
+import React, { useContext, Fragment, useEffect, useState }  from "react"
 import classes from "./CartButton.module.css"
 import CartIcon from "../Cart/CartIcon"
 import CartContext from "../../Store/CartContext"
 
 const CartButton = (props) => {
+    const [btnHighlight, setBtnHighlight] = useState(false)
     const cartCTX = useContext(CartContext)
 
     const numberOfCartItems = cartCTX.items.reduce((current, item) => {
         return current + item.amount
     }, 0)
 
+    const btnClasses = `${classes.button} ${classes.bump}`
+
+    useEffect(() => {}, [])
+
     return (
         <Fragment>
-            <button onClick={props.onClick} className={classes.button}>
+            <button onClick={props.onClick} className={btnClasses}>
                 <span className={classes.icon}>
                     <CartIcon/>
                 </span>
