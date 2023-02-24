6997207f23c34df144d50a12eb7c216a7985cf3e
Dan Stronge <dnlstronge@gmail.com>
2023-02-24 09:48:31 +0000

    [x] - import cartItems & css

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -6,9 +6,12 @@ import CartContext from "../../Store/CartContext"
 const Cart = (props) => {
   const cartCTX = useContext(CartContext)
 
+  const totalAmount = ` £${cartCTX.totalAmount.toFixed(2)}`
+  const hasItems = cartCTX.items.length > 0
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {cartCTX.map((item) => (
+      {cartCTX.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -23,11 +26,12 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onClose} className={classes['button--alt']}>Close</button>
-        <button className={classes.button}>Order</button>
+        {hasItems && 
+        <button className={classes.button}>Order</button>}
       </div>
     </Modal>
     </Fragment>
