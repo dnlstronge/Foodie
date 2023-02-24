2f00d56f7d5348de84291e9ba0462a9438a430b7
Dan Stronge <dnlstronge@gmail.com>
2023-02-24 10:43:34 +0000

    [x] change reducer logic to handle items of the same type

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -8,7 +8,7 @@ import CartContext from "../../Store/CartContext";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {};
