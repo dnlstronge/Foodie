e181825b35b6cbbe3211fda1622c89ef7474118c
Dan Stronge <dnlstronge@gmail.com>
2023-02-24 09:10:36 +0000

    [x] - number of items now shows in cart

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
+import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm"
+import CartContext from "../../../Store/CartContext"
 
 
 const MealItem = (props) => {
+    const cartCTX = useContext(CartContext)
     const price = `£${props.price.toFixed(2)}`
+
+    const addToCartHandler = (amount) => {
+      cartCTX.addItem({
+        id: props.id,
+        name: props.name,
+        amount: amount,
+        price: props.price
+      })
+    }
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
               <div className={classes.price}>{price}</div>
             </div>
             <div>
-              <MealItemForm id={props.id}/>
+              <MealItemForm onAddToCart={addToCartHandler} id={props.id}/>
               </div>
             
         </li>
