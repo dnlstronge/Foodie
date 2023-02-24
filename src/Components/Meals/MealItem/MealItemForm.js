d7c36804b8bb323d7a4cf40325bd467ba3928200
Dan Stronge <dnlstronge@gmail.com>
2023-02-24 08:53:41 +0000

    [x] - add state for error/validation

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,16 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true)
     const amountInputRef = useRef()
     const submitHandler = (e) => {
         e.preventDefault()
         const enteredAmount = amountInputRef.current.value
         const enteredAmountNumber = +enteredAmount
         if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid(false)
             return
         }
     }
