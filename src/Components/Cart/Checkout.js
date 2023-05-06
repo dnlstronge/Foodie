69dff9db4f0bbb5be1db68e6dcae6777f5a478ed
Dan Stronge <dnlstronge@gmail.com>
2023-05-06 10:31:17 +0100

    [x] - add template

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -4,9 +4,19 @@ import classes from "./Checkout.module.css"
 const Checkout = () => {
     return (
         <form>
-            <div className={classes.control}></div>
-            <label htmlFor="name">Your name</label>
-            <input id="name" type="text" />
+            <div className={classes.control}>
+                <label htmlFor="name">Your name</label>
+                <input id="name" type="text" /> 
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="street">Street</label>
+                <input id="street" type="text" /> 
+            </div>
+            <div className={classes.control}>
+                <label htmlFor="postcode">Postcode</label>
+                <input id="postcode" type="text" /> 
+            </div>
+           
         </form>
     )
 }
