6d2194b0e5d9093e4b9bd8a91b21f3fb3c89fa71
Dan Stronge <dnlstronge@gmail.com>
2023-05-06 10:36:22 +0100

    [x] - add form to cart

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -13,9 +13,14 @@ const Checkout = () => {
                 <input id="street" type="text" /> 
             </div>
             <div className={classes.control}>
+                <label htmlFor="city">City</label>
+                <input id="city" type="text" /> 
+            </div>
+            <div className={classes.control}>
                 <label htmlFor="postcode">Postcode</label>
                 <input id="postcode" type="text" /> 
             </div>
+            <button className={classes.btn}>Confirm</button>
            
         </form>
     )
