dc46dc57dbdd200858eca364d4840b2876218222
Dan Stronge <dnlstronge@gmail.com>
2023-05-09 08:31:11 +0100

    [x] - format

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -7,30 +7,33 @@ const isEmpty = val => val.trim().replace(/ /g, "") === ""
 const isOverSixChars = val => val.trim().replace(/ /g, "").length() >= 6
 
 const Checkout = (props) => {
+    /* form validity state */
+
+
     const nameInput = useRef()
     const streetInput = useRef()
     const postcodeInput = useRef()
     const cityInput = useRef()
 
 
-  const confirmHandler = (event) => {
-    event.preventDefault();
-    const enteredName = nameInput.current.value 
-    const enteredStreet = streetInput.current.value
-    const enteredPostcode = postcodeInput.current.value 
-    const enteredCity = cityInput.current.value
+    const confirmHandler = (event) => {
+        event.preventDefault();
+        const enteredName = nameInput.current.value 
+        const enteredStreet = streetInput.current.value
+        const enteredPostcode = postcodeInput.current.value 
+        const enteredCity = cityInput.current.value
 
-    /*validate data */
-    const enteredNameIsValid = !isEmpty(enteredName)
-    const enteredStreetIsValid = !isEmpty(enteredStreet)
-    const enteredCityIsValid = !isEmpty(enteredCity)
-    const enteredPostcodeIsValid = isOverSixChars(enteredPostcode)
+        /*validate data */
+        const enteredNameIsValid = !isEmpty(enteredName)
+        const enteredStreetIsValid = !isEmpty(enteredStreet)
+        const enteredCityIsValid = !isEmpty(enteredCity)
+        const enteredPostcodeIsValid = isOverSixChars(enteredPostcode)
 
-    const formIsValid = 
-        enteredNameIsValid &&
-        enteredStreetIsValid &&
-        enteredCityIsValid &&
-        enteredPostcodeIsValid
+        const formIsValid = 
+            enteredNameIsValid &&
+            enteredStreetIsValid &&
+            enteredCityIsValid &&
+            enteredPostcodeIsValid
 
    if(!formIsValid) {
     // handle error
