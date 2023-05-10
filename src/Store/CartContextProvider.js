77b0ffed6b1ff1c509216eb291424454c7f5f4fd
Dan Stronge <dnlstronge@gmail.com>
2023-05-10 10:24:19 +0100

    [x] - cart now clears on submit

--- src/Store/CartContextProvider.js
+++ src/Store/CartContextProvider.js
@@ -66,6 +66,8 @@ const defaultCartState = {
       cartReducer,
       defaultCartState
     );
+
+    /* handlers */
   
     const addItemToCartHandler = (item) => {
       dispatchCartAction({ type: 'ADD', item: item });
@@ -74,12 +76,17 @@ const defaultCartState = {
     const removeItemFromCartHandler = (id) => {
       dispatchCartAction({ type: 'REMOVE', id: id });
     };
+
+    const clearCartHandler = () => {
+      dispatchCartAction({ type: "CLEAR"})
+    }
   
     const cartContext = {
       items: cartState.items,
       totalAmount: cartState.totalAmount,
       addItem: addItemToCartHandler,
       removeItem: removeItemFromCartHandler,
+      clearCart: clearCartHandler
     };
   
     return (
