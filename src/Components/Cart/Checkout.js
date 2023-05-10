be6a8b514bb8321376c910759fa131dc7e467218
Dan Stronge <dnlstronge@gmail.com>
2023-05-10 08:11:17 +0100

    [x] - add conditional styles

--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -67,23 +67,23 @@ const Checkout = (props) => {
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${validState.name ? "" : classes.invalid}`}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInput} type='text' id='name' />
         {!validState.name && <p>Please enter a valid name</p>}
         
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${validState.street ? "" : classes.invalid}`}>
         <label htmlFor='street'>Street</label>
         <input ref={streetInput} type='text' id='street' />
         {!validState.street && <p>Please enter a valid street</p>}
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${validState.postcode ? "" : classes.invalid}`}>
         <label htmlFor='postal'>Postal Code</label>
         <input ref={postcodeInput} type='text' id='postal' />
         {!validState.postcode && <p>Please enter a valid postcode</p>}
       </div>
-      <div className={classes.control}>
+      <div className={`${classes.control} ${validState.city ? "" : classes.invalid}`}>
         <label htmlFor='city'>City</label>
         <input ref={cityInput} type='text' id='city' />
         {!validState.city && <p>Please enter a valid city</p>}
